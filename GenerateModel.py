c5b7f7332eda75a5c7046745900b1d62e03cb0cb
mdogan93 <mehmetdogan1993@gmail.com>
2018-01-17 00:05:20 +0300

    dataset and initial commit

---
+++ GenerateModel.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+
+
+
+
+#resources
+#https://kevinzakka.github.io/2016/07/13/k-nearest-neighbor/
+#https://github.com/mdogan93/GradProject/blob/master/preFinalTraining.py
+#http://archive.ics.uci.edu/ml/datasets/Activity+recognition+with+healthy+older+people+using+a+batteryless+wearable+sensor
+#https://www.kaggle.com/loveall/appliances-energy-prediction/data
+#https://openzeppelin.org/
+#http://hyperledger-fabric.readthedocs.io/en/release/blockchain.html
